80a74f7f0dbbc7a2aa8f78ffb4fd78166feffa7e
lgx <1347769076@qq.com>
2021-02-14 21:30:03 +0800

    提交了文档

--- 大事件项目/assets/js/login.js
+++ 大事件项目/assets/js/login.js
@@ -1,11 +1,25 @@
-$(function(){
-    
+$(function () {
+
+
+    var form = layui.form;
+ 
+
+    // layui 密码校验
+    form.verify({
+        username : [
+            /^[\S]{6,12}$/, '用户必须6到12位，且不能出现空格'
+        ],
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ]
+    });
+
     // 去注册/去登录
-    $('.toggle').click(function(){
+    $('.toggle').click(function () {
         $('#reg-module').toggle();
         $('#login-module').toggle();
     });
 
-    
+
 
 });
